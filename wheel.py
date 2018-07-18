e2b0dc4621500b1bb78d13557156bd7ea6c52076
deruga04 <derekli.3628@gmail.com>
2018-07-18 12:20:07 -0400

    Added cow script

---
+++ wheel.py
@@ -0,0 +1,20 @@
+import random
+
+xxx = random.choice(['subway',
+        'smoke\'s',
+        'wendys',
+        'jerk chicken',
+        'mandarin',
+        'bang me baby',
+        'popeyes',
+        'csgo'])
+
+
+print(' ' + '_' * (len(xxx) + 2) + ' ')
+print('< ' + xxx + ' >')
+print(' ' + '-' * (len(xxx) + 2) + ' ')
+print('        \\   ^__^')
+print('         \\  (oo)\\_______')
+print('            (__)\\       )\\/\\')
+print('                ||----w |')
+print('                ||     ||           Credits to cowsay')
